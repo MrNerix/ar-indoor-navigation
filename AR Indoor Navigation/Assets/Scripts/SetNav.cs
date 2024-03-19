79e333f6dcfd6da4bb6c14dbc11499d06b058929
MatasKa <matas.k2002@gmail.com>
2024-03-19 13:43:43 +0100

    Path length calculation function
    Added a function that calculates the path's length

--- AR Indoor Navigation/Assets/Scripts/SetNav.cs
+++ AR Indoor Navigation/Assets/Scripts/SetNav.cs
@@ -24,7 +24,8 @@ public class SetNav : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if((Input.touchCount > 0)&& (Input.GetTouch(0).phase == TouchPhase.Began)){
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
             lineToggle = !lineToggle;
         }
         if (lineToggle)
@@ -33,7 +34,25 @@ public class SetNav : MonoBehaviour
             line.positionCount = path.corners.Length;
             line.SetPositions(path.corners);
             line.enabled = true;
+
+            // Calculate and output the length of the line
+            //float length = CalculateLineLength(path.corners);
+            //Debug.Log("Line length: " + length);
         }
-        
+        else
+        {
+            line.enabled = false;
+        }
+    }
+
+    // Method to calculate the length of the line based on its corners
+    private float CalculateLineLength(Vector3[] corners)
+    {
+        float length = 0f;
+        for (int i = 0; i < corners.Length - 1; i++)
+        {
+            length += Vector3.Distance(corners[i], corners[i + 1]);
+        }
+        return length;
     }
 }
