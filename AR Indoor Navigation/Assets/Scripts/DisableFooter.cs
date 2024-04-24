afea0e8b4ae8435f163f2f8ecec489232f2c9ced
Juste <90906175+JusteBuu@users.noreply.github.com>
2024-04-24 11:29:17 +0200

    added some UI in navigation scene as well as some functionality to the buttons

---
+++ AR Indoor Navigation/Assets/Scripts/DisableFooter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFooter : MonoBehaviour
+{
+    public GameObject footerExpanded;
+    public GameObject footerSmall;
+
+    public void Start()
+    {
+        footerSmall.SetActive(false);
+        //footerExpanded.SetActive(true);
+    }
+    
+    public void CompressButton()
+    {
+        if (footerExpanded.activeInHierarchy == true)
+        {
+            footerExpanded.SetActive(false);
+            footerSmall.SetActive(true);
+        }
+    }
+
+    public void ShowMiniMap()
+    {
+        footerExpanded.SetActive(true);
+        footerSmall.SetActive(false);
+    }
+}
