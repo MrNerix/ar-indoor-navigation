9da59f7473bf1c5799e4193af7bbddd0a32b4a52
MatasKa <matas.k2002@gmail.com>
2024-04-24 09:45:05 +0200

    Created a Filter script

---
+++ AR Indoor Navigation/Assets/Scripts/Filter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Filter : MonoBehaviour
+{
+    // Lists to store classrooms and group rooms
+    public List<string> classrooms = new List<string>();
+    public List<string> groupRooms = new List<string>();
+    public List<string> wc = new List<string>();
+    public List<string> wcHandicapped = new List<string>();
+    public List<string> stairs = new List<string>();
+    public List<string> elevators = new List<string>();
+    public List<string> coffeeSpots = new List<string>();
+    public List<string> printers = new List<string>();
+    public List<string> lockers = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
