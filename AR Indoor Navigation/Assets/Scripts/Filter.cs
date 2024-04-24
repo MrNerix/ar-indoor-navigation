2f41dd0d4068eb05b49cf75824df304ee14de50c
MatasKa <matas.k2002@gmail.com>
2024-04-24 10:30:26 +0200

    Updated the type dropdown list

--- AR Indoor Navigation/Assets/Scripts/Filter.cs
+++ AR Indoor Navigation/Assets/Scripts/Filter.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Filter : MonoBehaviour
 {
     // Lists (used before we implement a database)
-    public List<string> classrooms = new List<string>();
-    public List<string> groupRooms = new List<string>();
-    public List<string> wc = new List<string>();
-    public List<string> wcHandicapped = new List<string>();
-    public List<string> stairs = new List<string>();
-    public List<string> elevators = new List<string>();
-    public List<string> coffeeSpots = new List<string>();
-    public List<string> printers = new List<string>();
-    public List<string> lockers = new List<string>();
+    private List<string> classrooms = new List<string>();
+    private List<string> groupRooms = new List<string>();
+    private List<string> wc = new List<string>();
+    private List<string> wcHandicapped = new List<string>();
+    private List<string> stairs = new List<string>();
+    private List<string> elevators = new List<string>();
+    private List<string> coffeeSpots = new List<string>();
+    private List<string> printers = new List<string>();
+    private List<string> lockers = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
