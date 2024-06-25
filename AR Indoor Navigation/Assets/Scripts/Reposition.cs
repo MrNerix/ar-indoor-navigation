fe6b9ac3a56d26b499cfcbcc0e45da3a92880d12
MatasKa <matas.k2002@gmail.com>
2024-06-25 14:56:56 +0200

    Added a line renderer to see the closest path back to the navmesh

--- AR Indoor Navigation/Assets/Scripts/Reposition.cs
+++ AR Indoor Navigation/Assets/Scripts/Reposition.cs
@@ -6,21 +6,36 @@ public class Reposition : MonoBehaviour
     public float speed = 20.0f; // Speed at which the object moves
     public GameObject user;
     //public GameObject Line;
-    public LineRenderer lineRenderer; // Assume you have a line renderer to draw the path
-
+    public LineRenderer NavigationLineRenderer; // Assume you have a line renderer to draw the path
+    public LineRenderer lineRenderer;
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        //NavigationLineRenderer = GetComponent<LineRenderer>();
+        SetupLineRenderer();
+
     }
     void Update()
     {
         float distance;
         Vector3 nearestPosition = FindNearestNavMeshPosition(transform.position, out distance);
         Debug.Log("Distance to nearest NavMesh position: " + distance);
+        DrawPathToNavMesh(nearestPosition);
     }
+    void SetupLineRenderer()
+    {
+        //lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.5f;
+        lineRenderer.endWidth = 0.5f;
+    }
+    void DrawPathToNavMesh(Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, targetPosition);
+    }
+
     bool IsLineDrawn()
     {
-        return lineRenderer.positionCount > 1;
+        return NavigationLineRenderer.positionCount > 1;
     }
     public Vector3 FindNearestNavMeshPosition(Vector3 position, out float distance)
     {
@@ -33,7 +48,7 @@ public class Reposition : MonoBehaviour
         {
             Vector3 targetPosition = new Vector3(hit.position.x, transform.position.y, hit.position.z); // Ignore Y-axis for the movement
             distance = Vector3.Distance(new Vector3(position.x, 0, position.z), new Vector3(hit.position.x, 0, hit.position.z));
-            if (IsLineDrawn())
+            if (!IsLineDrawn())
             {
                 user.transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
             }
